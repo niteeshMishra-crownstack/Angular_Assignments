d4d9c0fba0fec733c62d5737b1195744e3b14a9e
Niteesh Mishra <niteesh.mishra29@gmail.com>
2021-04-29 20:24:44 +0530

    budget app

---
+++ budget-app/src/app/budget-item-list/budget-list-card/budget-list-card.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { BudgetListCardComponent } from './budget-list-card.component';
+
+describe('BudgetListCardComponent', () => {
+  let component: BudgetListCardComponent;
+  let fixture: ComponentFixture<BudgetListCardComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ BudgetListCardComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BudgetListCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
